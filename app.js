d6efbef97903947ba381fb563374662a8a1b0828
luciano de franca <lucianodefranca86@gmail.com>
2022-02-07 22:13:14 -0300

    Exibindo lista dados na tela

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 class Despesa {
-
     constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano;
         this.mes = mes;
@@ -11,7 +10,7 @@ class Despesa {
 
     validarDados() {
         for (const x in this) {
-            if (this[x] === undefined || this[x] === '' || this[x] === null) {
+            if (this[x] === undefined || this[x] === "" || this[x] === null) {
                 return false;
             }
         }
@@ -20,43 +19,37 @@ class Despesa {
 }
 
 class BD {
-
     constructor() {
-
-        let id = localStorage.getItem('id');
+        let id = localStorage.getItem("id");
 
         if (id === null) {
-            localStorage.setItem('id', 0);
+            localStorage.setItem("id", 0);
         }
     }
 
     proximoId() {
-
-        let proximoId = localStorage.getItem('id');
+        let proximoId = localStorage.getItem("id");
         return parseInt(proximoId) + 1;
     }
 
     gravar(obj) {
-
         let id = this.proximoId();
 
         localStorage.setItem(id, JSON.stringify(obj));
 
-        localStorage.setItem('id', id);
+        localStorage.setItem("id", id);
     }
 
     recuperarTodosRegistros() {
-
         let despesas = Array();
 
-        let id = localStorage.getItem('id');
+        let id = localStorage.getItem("id");
 
         for (let i = 1; i <= id; i++) {
-
             let despesa = JSON.parse(localStorage.getItem(i));
 
             if (despesa === null) {
-                continue
+                continue;
             }
 
             despesas.push(despesa);
@@ -67,15 +60,13 @@ class BD {
 
 let bd = new BD();
 
-
 function cadastrarDespesa() {
-
-    let ano = document.getElementById('ano');
-    let mes = document.getElementById('mes');
-    let dia = document.getElementById('dia');
-    let tipo = document.getElementById('tipo');
-    let descricao = document.getElementById('descricao');
-    let valor = document.getElementById('valor');
+    let ano = document.getElementById("ano");
+    let mes = document.getElementById("mes");
+    let dia = document.getElementById("dia");
+    let tipo = document.getElementById("tipo");
+    let descricao = document.getElementById("descricao");
+    let valor = document.getElementById("valor");
 
     let despesa = new Despesa(
         ano.value,
@@ -89,32 +80,63 @@ function cadastrarDespesa() {
     if (despesa.validarDados()) {
         bd.gravar(despesa);
 
-        document.getElementById('modal_div').className = "modal-header text-success";
-        document.getElementById('titulo_modal').innerHTML = "Registro inserido com sucesso!";
-        document.getElementById('modal_conteudo').innerHTML = "Despesa foi cadastrada com sucesso";
-        document.getElementById('btn_modal').innerHTML = 'Voltar';
-        document.getElementById('btn_modal').className = 'btn btn-success';
+        document.getElementById("modal_div").className =
+            "modal-header text-success";
+        document.getElementById("titulo_modal").innerHTML =
+            "Registro inserido com sucesso!";
+        document.getElementById("modal_conteudo").innerHTML =
+            "Despesa foi cadastrada com sucesso";
+        document.getElementById("btn_modal").innerHTML = "Voltar";
+        document.getElementById("btn_modal").className = "btn btn-success";
 
-
-        $('#modalRegistraGravacao').modal('show');
+        $("#modalRegistraGravacao").modal("show");
     } else {
+        document.getElementById("modal_div").className = "modal-header text-danger";
+        document.getElementById("titulo_modal").innerHTML =
+            "Erro na inclusão do registro!";
+        document.getElementById("modal_conteudo").innerHTML =
+            "Existem campos obrigatórios não preenchidos!";
+        document.getElementById("btn_modal").innerHTML = "Voltar e corrigir";
+        document.getElementById("btn_modal").className = "btn btn-danger";
 
-        document.getElementById('modal_div').className = "modal-header text-danger";
-        document.getElementById('titulo_modal').innerHTML = "Erro na inclusão do registro!";
-        document.getElementById('modal_conteudo').innerHTML = "Existem campos obrigatórios não preenchidos!";
-        document.getElementById('btn_modal').innerHTML = 'Voltar e corrigir';
-        document.getElementById('btn_modal').className = 'btn btn-danger';
-
-        $('#modalRegistraGravacao').modal('show');
+        $("#modalRegistraGravacao").modal("show");
     }
-
 }
 
 function carregarListaDespesas() {
-
     let despesas = Array();
 
     despesas = bd.recuperarTodosRegistros();
+
+    let listaDespesas = document.getElementById("lista_despesas");
+
+    despesas.forEach((obj) => {
+        let linha = listaDespesas.insertRow();
+
+        linha.insertCell(0).innerHTML = `${obj.dia}/${obj.mes}/${obj.ano}`;
+
+        switch (obj.tipo) {
+            case "1":
+                obj.tipo = "Alimentação";
+                break;
+            case "2":
+                obj.tipo = "Educação";
+                break;
+            case "3":
+                obj.tipo = "Lazer";
+                break;
+            case "4":
+                obj.tipo = "Saúde";
+                break;
+            case "5":
+                obj.tipo = "Transporte";
+                break;
+        }
+
+        linha.insertCell(1).innerHTML = obj.tipo;
+        linha.insertCell(2).innerHTML = obj.descricao;
+        linha.insertCell(3).innerHTML = obj.valor;
+    });
 
     console.log(despesas);
 }
